12262a010132d1ff99b53791d53be2dd1c59cd2d
abdel-17 <abdelrahmanb015@gmail.com>
2024-02-16 15:02:33 +0200

    remove pp

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,3 @@
-export { melt } from "@melt-ui/svelte";
 export * from "./builders/label";
 export * from "./builders/toggle";
 export * from "./builders/tooltip";
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import { sequence } from "@melt-ui/pp";
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess()]),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 	},
