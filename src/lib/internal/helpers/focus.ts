b73e37c131af8589bc1719bdfb8e90f422a084d0
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-08 07:34:11 +0200

    fix types

--- src/lib/internal/helpers/focus.ts
+++ src/lib/internal/helpers/focus.ts
@@ -1,8 +1,8 @@
 import { isFunction, isHTMLElement } from "$lib/internal/helpers/index.js";
 import { tick } from "svelte";
 
-export type FocusTarget = string | HTMLElement | null;
-export type FocusProp = FocusTarget | ((defaultEl: HTMLElement | null) => FocusTarget) | undefined;
+export type FocusTarget = string | HTMLElement | null | undefined;
+export type FocusProp = FocusTarget | ((defaultEl: HTMLElement | null) => FocusTarget);
 
 export type HandleFocusArgs = {
 	prop: FocusProp;
